e31015aaf34951677c61f294c7a85fdafd5aa0bb
Dave Cheney <dave@cheney.net>
2013-11-07 11:31:46 +1100

    Use forked fs.Walker

--- client.go
+++ client.go
@@ -4,8 +4,9 @@ import (
 	"io"
 	"os"
 	"path"
-	"path/filepath"
 	"sync"
+
+	"github.com/davecheney/fs"
 
 	"code.google.com/p/go.crypto/ssh"
 )
@@ -91,9 +92,8 @@ func (c *Client) recvVersion() error {
 }
 
 // Walk returns a new Walker rooted at root.
-func (c *Client) Walk(root string) *Walker {
-	info, err := c.Lstat(root)
-	return &Walker{c: c, stack: []item{{root, info, err}}}
+func (c *Client) Walk(root string) *fs.Walker {
+	return fs.WalkFunc(root, c.Lstat, c.readDir)
 }
 
 func (c *Client) readDir(p string) ([]os.FileInfo, error) {
@@ -536,81 +536,6 @@ func (f *File) Write(b []byte) (int, error) {
 	n, err := f.c.writeAt(f.handle, f.offset, b)
 	f.offset += uint64(n)
 	return int(n), err
-}
-
-// Walker provides a convenient interface for iterating over the
-// descendants of a filesystem path.
-// Successive calls to the Step method will step through each
-// file or directory in the tree, including the root. The files
-// are walked in lexical order, which makes the output deterministic
-// but means that for very large directories Walker can be inefficient.
-// Walker does not follow symbolic links.
-type Walker struct {
-	c       *Client
-	cur     item
-	stack   []item
-	descend bool
-}
-
-// Err returns the error, if any, for the most recent attempt
-// by Step to visit a file or directory. If a directory has
-// an error, w will not descend into that directory.
-func (w *Walker) Err() error {
-	return w.cur.err
-}
-
-// Path returns the path to the most recent file or directory
-// visited by a call to Step. It contains the argument to Walk
-// as a prefix; that is, if Walk is called with "dir", which is
-// a directory containing the file "a", Path will return "dir/a".
-func (w *Walker) Path() string {
-	return w.cur.path
-}
-
-// Stat returns info for the most recent file or directory
-// visited by a call to Step.
-func (w *Walker) Stat() os.FileInfo {
-	return w.cur.info
-}
-
-// Step advances the Walker to the next file or directory,
-// which will then be available through the Path, Stat,
-// and Err methods.
-// It returns false when the walk stops at the end of the tree.
-func (w *Walker) Step() bool {
-	if w.descend && w.cur.err == nil && w.cur.info.IsDir() {
-		list, err := w.c.readDir(w.cur.path)
-		if err != nil {
-			w.cur.err = err
-			w.stack = append(w.stack, w.cur)
-		} else {
-			for i := len(list) - 1; i >= 0; i-- {
-				path := filepath.Join(w.cur.path, list[i].Name())
-				w.stack = append(w.stack, item{path, list[i], nil})
-			}
-		}
-	}
-
-	if len(w.stack) == 0 {
-		return false
-	}
-	i := len(w.stack) - 1
-	w.cur = w.stack[i]
-	w.stack = w.stack[:i]
-	w.descend = true
-	return true
-}
-
-// SkipDir causes the currently visited directory to be skipped.
-// If w is not on a directory, SkipDir has no effect.
-func (w *Walker) SkipDir() {
-	w.descend = false
-}
-
-type item struct {
-	path string
-	info os.FileInfo
-	err  error
 }
 
 // okOrErr returns nil if Err.Code is SSH_FX_OK, otherwise it returns the error.
