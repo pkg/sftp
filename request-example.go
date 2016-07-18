0b181907403c38e7163e09382a02e88d06372dde
John Eikenberry <jae@zhar.net>
2016-07-18 15:53:13 -0700

    switch fs from tree to flat map
    Simplify the example handlers FS-like struct by changing it from a tree
    to a map.

--- request-example.go
+++ request-example.go
@@ -6,17 +6,143 @@ import (
 	"io"
 	"os"
 	"path/filepath"
-	"strings"
+	"syscall"
 	"time"
 )
 
 var _ = fmt.Println
 
+func InMemHandler() Handlers {
+	root := &root{
+		files: make(map[string]*memFile),
+	}
+	root.memFile = newMemFile("/", true)
+	return Handlers{root, root, root, root}
+}
+
+//
+type root struct {
+	*memFile
+	files map[string]*memFile
+}
+
+func (r *root) fetch(path string) (*memFile, error) {
+	fmt.Println("fetch", r.files)
+	if path == "/" {
+		return r.memFile, nil
+	}
+	if file, ok := r.files[path]; ok {
+		return file, nil
+	}
+	return nil, os.ErrNotExist
+}
+
+// Handlers
+func (fs *root) Fileread(r *Request) (io.Reader, error) {
+	file, err := fs.fetch(r.Filepath)
+	if err != nil {
+		return nil, err
+	}
+	if file.symlink != "" {
+		file, err = fs.fetch(file.symlink)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return file.Reader()
+}
+
+func (fs *root) Filewrite(r *Request) (io.Writer, error) {
+	file, err := fs.fetch(r.Filepath)
+	if err == os.ErrNotExist {
+		dir, err := fs.fetch(filepath.Dir(r.Filepath))
+		if err != nil {
+			return nil, err
+		}
+		if !dir.isdir {
+			return nil, os.ErrInvalid
+		}
+		file = newMemFile(r.Filepath, false)
+		fs.files[r.Filepath] = file
+	}
+	return file.Writer()
+}
+
+func (fs *root) Filecmd(r *Request) error {
+	switch r.Method {
+	case "SetStat":
+		return nil
+	case "Rename":
+		file, err := fs.fetch(r.Filepath)
+		if err != nil {
+			return err
+		}
+		fs.files[r.Target] = file
+		delete(fs.files, r.Filepath)
+	case "Rmdir", "Remove":
+		_, err := fs.fetch(filepath.Dir(r.Filepath))
+		if err != nil {
+			return err
+		}
+		delete(fs.files, r.Filepath)
+	case "Mkdir":
+		_, err := fs.fetch(filepath.Dir(r.Filepath))
+		if err != nil {
+			return err
+		}
+		fs.files[r.Filepath] = newMemFile(r.Filepath, true)
+	case "Symlink":
+		fmt.Println("ln -s", r.Filepath, r.Target)
+		_, err := fs.fetch(r.Filepath)
+		if err != nil {
+			return err
+		}
+		link := newMemFile(r.Target, false)
+		link.symlink = r.Filepath
+		fs.files[r.Target] = link
+	}
+	return nil
+}
+
+func (fs *root) Fileinfo(r *Request) ([]os.FileInfo, error) {
+	switch r.Method {
+	case "List":
+		list := []os.FileInfo{}
+		fmt.Println("ls", r.Filepath)
+		for fn, fi := range fs.files {
+			if filepath.Dir(fn) == r.Filepath {
+				fmt.Println(fn, fi.Name())
+				list = append(list, fi)
+			}
+		}
+		return list, nil
+	case "Stat":
+		file, err := fs.fetch(r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+		return []os.FileInfo{file}, nil
+	case "Readlink":
+		file, err := fs.fetch(r.Filepath)
+		if err != nil {
+			return nil, err
+		}
+		if file.symlink != "" {
+			file, err = fs.fetch(file.symlink)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return []os.FileInfo{file}, nil
+	}
+	return nil, nil
+}
+
+// Implements os.FileInfo interface
 type memFile struct {
 	name    string
 	content []byte
 	modtime time.Time
-	files   map[string]*memFile
 	symlink string
 	isdir   bool
 }
@@ -25,29 +151,27 @@ func newMemFile(name string, isdir bool) *memFile {
 	return &memFile{
 		name:    name,
 		modtime: time.Now(),
-		files:   make(map[string]*memFile),
 		isdir:   isdir,
 	}
 }
 
-func InMemHandler() Handlers {
-	root := newMemFile("/", true)
-	return Handlers{root, root, root, root}
-}
-
 // Have memFile fulfill os.FileInfo interface
-func (f *memFile) Name() string { return f.name }
-func (f *memFile) Size() int64  { return int64(len(f.content)) }
+func (f *memFile) Name() string {
+	return filepath.Base(f.name)
+}
+func (f *memFile) Size() int64 { return int64(len(f.content)) }
 func (f *memFile) Mode() os.FileMode {
-	ret := os.ModePerm
+	ret := os.FileMode(0644)
 	if f.isdir {
-		ret = ret | os.ModeDir
+		ret = os.FileMode(0755) | os.ModeDir
 	}
 	return ret
 }
 func (f *memFile) ModTime() time.Time { return time.Now() }
 func (f *memFile) IsDir() bool        { return f.isdir }
-func (f *memFile) Sys() interface{}   { return nil }
+func (f *memFile) Sys() interface{} {
+	return syscall.Stat_t{Uid: 65534, Gid: 65534}
+}
 
 // Read/Write
 func (f *memFile) Reader() (io.Reader, error) {
@@ -66,140 +190,4 @@ func (f *memFile) Writer() (io.Writer, error) {
 func (f *memFile) Write(p []byte) (int, error) {
 	f.content = append(f.content, p...)
 	return len(p), nil
-}
-
-// Filesystemy things
-func (f *memFile) fetch(path string) (*memFile, error) {
-	var file *memFile = f
-	for _, name := range strings.Split(path, "/") {
-		fmt.Print(name, " - ")
-		var ok bool
-		switch name {
-		case "/":
-		default:
-			fmt.Println(file.files)
-			if file, ok = file.files[name]; !ok {
-				return nil, os.ErrNotExist
-			}
-		}
-	}
-	return file, nil
-}
-
-// Handlers
-func (f *memFile) Fileread(r *Request) (io.Reader, error) {
-	file, err := f.fetch(r.Filepath)
-	if err != nil {
-		return nil, err
-	}
-	if file.symlink != "" {
-		file, err = f.fetch(file.symlink)
-		if err != nil {
-			return nil, err
-		}
-	}
-	return file.Reader()
-}
-
-func (f *memFile) Filewrite(r *Request) (io.Writer, error) {
-	file, err := f.fetch(r.Filepath)
-	if err == os.ErrNotExist {
-		dir, err := f.fetch(filepath.Dir(r.Filepath))
-		if !dir.isdir {
-			return nil, os.ErrInvalid
-		}
-		if err != nil {
-			return nil, err
-		}
-		name := filepath.Base(r.Filepath)
-		file = newMemFile(name, false)
-		dir.files[name] = file
-	}
-	return file.Writer()
-}
-
-func (f *memFile) Filecmd(r *Request) error {
-	switch r.Method {
-	case "SetStat":
-		return nil
-	case "Rename":
-		filename := filepath.Base(r.Filepath)
-		orig_dir, err := f.fetch(filepath.Dir(r.Filepath))
-		if err != nil {
-			return err
-		}
-		dest, err := f.fetch(r.Target)
-		if err != nil {
-			return err
-		}
-		if !dest.isdir {
-			return os.ErrInvalid
-		}
-		dest.files[filename] = orig_dir.files[filename]
-		delete(orig_dir.files, filename)
-	case "Rmdir", "Remove":
-		parent, err := f.fetch(filepath.Dir(r.Filepath))
-		if err != nil {
-			return err
-		}
-		delete(parent.files, filepath.Base(r.Filepath))
-	case "Mkdir":
-		parent, err := f.fetch(filepath.Dir(r.Filepath))
-		if err != nil {
-			return err
-		}
-		name := filepath.Base(r.Filepath)
-		file := newMemFile(name, true)
-		parent.files[name] = file
-	case "Symlink":
-		fmt.Println("ln -s", r.Filepath, r.Target)
-		parent, err := f.fetch(filepath.Dir(r.Filepath))
-		if err != nil {
-			return err
-		}
-		name := filepath.Base(r.Target)
-		file := newMemFile(name, false)
-		file.symlink = r.Filepath
-		parent.files[name] = file
-	}
-	return nil
-}
-
-func (f *memFile) Fileinfo(r *Request) ([]os.FileInfo, error) {
-	switch r.Method {
-	case "List":
-		file, err := f.fetch(r.Filepath)
-		if err != nil {
-			return nil, err
-		}
-		if file.isdir {
-			list := make([]os.FileInfo, 0, len(file.files))
-			for _, val := range file.files {
-				fmt.Println(val)
-				list = append(list, val)
-			}
-			return list, nil
-		} else {
-			return []os.FileInfo{file}, nil
-		}
-	case "Stat":
-		file, err := f.fetch(r.Filepath)
-		if err != nil {
-			return nil, err
-		}
-		return []os.FileInfo{file}, nil
-	case "Readlink":
-		file, err := f.fetch(r.Filepath)
-		if err != nil {
-			return nil, err
-		}
-		if len(file.symlink) > 0 {
-			file, err = f.fetch(file.symlink)
-			if err != nil {
-				return nil, err
-			}
-		}
-		return []os.FileInfo{file}, nil
-	}
-	return nil, nil
 }
