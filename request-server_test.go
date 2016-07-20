f2d2606fc7ff0a3c270c40a92bba28d32e03bf20
John Eikenberry <jae@zhar.net>
2016-07-20 16:49:00 -0700

    Add integration tests using example backend

--- request-server_test.go
+++ request-server_test.go
@@ -7,37 +7,79 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func clientRequestServerPair(t *testing.T) (*Client, *RequestServer) {
+type csPair struct {
+	cli *Client
+	svr *RequestServer
+}
+
+// these must be closed in order, else client.Close will hang
+func (cs csPair) Close() {
+	cs.svr.Close()
+	cs.cli.Close()
+}
+
+func clientRequestServerPair(t *testing.T) *csPair {
 	cr, sw := io.Pipe()
 	sr, cw := io.Pipe()
+	handlers := InMemHandler()
 	server, err := NewRequestServer(struct {
 		io.Reader
 		io.WriteCloser
-	}{sr, sw}, Handlers{})
-	if err != nil {
-		t.Fatal(err)
-	}
+	}{sr, sw}, handlers)
+	if err != nil { t.Fatal(err) }
 	go server.Serve()
 	client, err := NewClientPipe(cr, cw)
-	if err != nil {
-		t.Fatalf("%+v\n", err)
-	}
-	return client, server
+	if err != nil { t.Fatalf("%+v\n", err) }
+	return &csPair{client, server}
 }
 
 func TestRequestCache(t *testing.T) {
-	_, rs := clientRequestServerPair(t)
+	p := clientRequestServerPair(t)
+	defer p.Close()
 	foo := &Request{Filepath: "foo"}
 	bar := &Request{Filepath: "bar"}
-	rs.nextRequest(foo)
-	rs.nextRequest(bar)
-	assert.Len(t, rs.openRequests, 2)
-	_foo, ok := rs.getRequest("foo")
+	p.svr.nextRequest(foo)
+	p.svr.nextRequest(bar)
+	assert.Len(t, p.svr.openRequests, 2)
+	_foo, ok := p.svr.getRequest("foo")
 	assert.Equal(t, foo, _foo)
 	assert.True(t, ok)
-	_, ok = rs.getRequest("zed")
+	_, ok = p.svr.getRequest("zed")
 	assert.False(t, ok)
-	rs.closeRequest("foo")
-	rs.closeRequest("bar")
-	assert.Len(t, rs.openRequests, 0)
+	p.svr.closeRequest("foo")
+	p.svr.closeRequest("bar")
+	assert.Len(t, p.svr.openRequests, 0)
+}
+
+func putTestFile(cli *Client, path, content string) (int, error) {
+	w, err := cli.Create(path)
+	if err == nil {
+		defer w.Close()
+		return w.Write([]byte(content))
+	}
+	return 0, err
+}
+
+func TestRequestWrite(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	n, err := putTestFile(p.cli, "/foo", "hello")
+	if err != nil { t.Fatal(err) }
+	assert.Equal(t, 5, n)
+}
+
+func TestRequestRead(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	_, err := putTestFile(p.cli, "/foo", "hello")
+	assert.Nil(t, err)
+	rf, err := p.cli.Open("/foo")
+	assert.Nil(t, err)
+	defer rf.Close()
+	// defer rf.Close()
+	contents := make([]byte, 5)
+	n, err := rf.Read(contents)
+	if err != nil && err != io.EOF { t.Fatalf("err: %v", err) }
+	assert.Equal(t, 5, n)
+	assert.Equal(t, "hello", string(contents[0:5]))
 }
