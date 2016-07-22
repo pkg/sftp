5eea9d6d8c0fb9d90d9400d3b2bb42cd6da7fb02
John Eikenberry <jae@zhar.net>
2016-07-22 15:16:25 -0700

    'integration' tests using in-mem request example backend

--- request-server_test.go
+++ request-server_test.go
@@ -1,11 +1,16 @@
 package sftp
 
 import (
+	"fmt"
 	"io"
+	"os"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var _ = fmt.Print
 
 type csPair struct {
 	cli *Client
@@ -16,6 +21,10 @@ type csPair struct {
 func (cs csPair) Close() {
 	cs.svr.Close()
 	cs.cli.Close()
+}
+
+func (cs csPair) testHandler() *root {
+	return cs.svr.Handlers.FileGet.(*root)
 }
 
 func clientRequestServerPair(t *testing.T) *csPair {
@@ -60,14 +69,33 @@ func putTestFile(cli *Client, path, content string) (int, error) {
 	return 0, err
 }
 
+// needs fail check
 func TestRequestWrite(t *testing.T) {
 	p := clientRequestServerPair(t)
 	defer p.Close()
 	n, err := putTestFile(p.cli, "/foo", "hello")
-	if err != nil { t.Fatal(err) }
+	assert.Nil(t, err)
 	assert.Equal(t, 5, n)
+	r := p.testHandler()
+	f, err := r.fetch("/foo")
+	assert.Nil(t, err)
+	assert.False(t, f.isdir)
+	assert.Equal(t, f.content, []byte("hello"))
 }
 
+// needs fail check
+func TestRequestFilename(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	_, err := putTestFile(p.cli, "/foo", "hello")
+	assert.Nil(t, err)
+	r := p.testHandler()
+	f, err := r.fetch("/foo")
+	assert.Nil(t, err)
+	assert.Equal(t, f.Name(), "foo")
+}
+
+// needs fail check
 func TestRequestRead(t *testing.T) {
 	p := clientRequestServerPair(t)
 	defer p.Close()
@@ -76,10 +104,92 @@ func TestRequestRead(t *testing.T) {
 	rf, err := p.cli.Open("/foo")
 	assert.Nil(t, err)
 	defer rf.Close()
-	// defer rf.Close()
 	contents := make([]byte, 5)
 	n, err := rf.Read(contents)
 	if err != nil && err != io.EOF { t.Fatalf("err: %v", err) }
 	assert.Equal(t, 5, n)
 	assert.Equal(t, "hello", string(contents[0:5]))
+}
+
+// needs fail check
+func TestRequestOpen(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	fh, err := p.cli.Open("foo")
+	assert.Nil(t, err)
+	err = fh.Close()
+	assert.Nil(t, err)
+}
+
+// needs fail check
+func TestRequestMkdir(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	err := p.cli.Mkdir("/foo")
+	assert.Nil(t, err)
+	r := p.testHandler()
+	f, err := r.fetch("/foo")
+	assert.Nil(t, err)
+	assert.True(t, f.isdir)
+}
+
+// needs fail check
+func TestRequestRemove(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	_, err := putTestFile(p.cli, "/foo", "hello")
+	assert.Nil(t, err)
+	r := p.testHandler()
+	_, err = r.fetch("/foo")
+	assert.Nil(t, err)
+	err = p.cli.Remove("/foo")
+	assert.Nil(t, err)
+	_, err = r.fetch("/foo")
+	assert.Equal(t, err, os.ErrNotExist)
+}
+
+// needs fail check
+func TestRequestRename(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	_, err := putTestFile(p.cli, "/foo", "hello")
+	assert.Nil(t, err)
+	r := p.testHandler()
+	_, err = r.fetch("/foo")
+	assert.Nil(t, err)
+	err = p.cli.Rename("/foo", "/bar")
+	assert.Nil(t, err)
+	_, err = r.fetch("/bar")
+	assert.Nil(t, err)
+	_, err = r.fetch("/foo")
+	assert.Equal(t, err, os.ErrNotExist)
+}
+
+func TestRequestStat(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	_, err := putTestFile(p.cli, "/foo", "hello")
+	assert.Nil(t, err)
+	fi, err := p.cli.Stat("/foo")
+	assert.Equal(t, fi.Name(), "foo")
+	assert.Equal(t, fi.Size(), int64(5))
+	assert.Equal(t, fi.Mode(), os.FileMode(0644))
+	fstat := fi.Sys().(*FileStat)
+	assert.Equal(t, fstat.UID, uint32(65534))
+	assert.Equal(t, fstat.GID, uint32(65534))
+}
+
+func TestRequestStatFail(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
+	fi, err := p.cli.Stat("/foo")
+	assert.Nil(t, fi)
+	fmt.Println(err, reflect.TypeOf(err))
+	assert.True(t, os.IsNotExist(err))
+}
+
+// {sym,read}link and readdir left
+func TestRequest(t *testing.T) {
+	p := clientRequestServerPair(t)
+	defer p.Close()
 }
