352ba90280c66632269ac886a7442bae99067a9b
John Eikenberry <jae@zhar.net>
2016-08-19 11:29:40 -0700

    use proper newRequest in test

--- request-server_test.go
+++ request-server_test.go
@@ -79,8 +79,8 @@ func TestRequestSplitWrite(t *testing.T) {
 func TestRequestCache(t *testing.T) {
 	p := clientRequestServerPair(t)
 	defer p.Close()
-	foo := Request{Filepath: "foo"}
-	bar := Request{Filepath: "bar"}
+	foo := newRequest("foo")
+	bar := newRequest("bar")
 	fh := p.svr.nextRequest(foo)
 	bh := p.svr.nextRequest(bar)
 	assert.Len(t, p.svr.openRequests, 2)
