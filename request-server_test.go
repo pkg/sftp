fef262888716a6116d23debbb0ce61df9cb75c58
Cassondra Foesch <puellanivis@gmail.com>
2020-10-28 20:20:28 +0000

    remove write/read race condition

--- request-server_test.go
+++ request-server_test.go
@@ -39,11 +39,14 @@ const sock = "/tmp/rstest.sock"
 func clientRequestServerPair(t *testing.T) *csPair {
 	skipIfWindows(t)
 	skipIfPlan9(t)
-	ready := make(chan bool)
+
+	ready := make(chan struct{})
+	canReturn := make(chan struct{})
 	os.Remove(sock) // either this or signal handling
 	pair := &csPair{
 		svrResult: make(chan error, 1),
 	}
+
 	var server *RequestServer
 	go func() {
 		l, err := net.Listen("unix", sock)
@@ -51,26 +54,37 @@ func clientRequestServerPair(t *testing.T) *csPair {
 			// neither assert nor t.Fatal reliably exit before Accept errors
 			panic(err)
 		}
-		ready <- true
+
+		close(ready)
+
 		fd, err := l.Accept()
 		require.NoError(t, err)
+
 		handlers := InMemHandler()
 		var options []RequestServerOption
 		if *testAllocator {
 			options = append(options, WithRSAllocator())
 		}
+
 		server = NewRequestServer(fd, handlers, options...)
+		close(canReturn)
+
 		err = server.Serve()
 		pair.svrResult <- err
 	}()
+
 	<-ready
 	defer os.Remove(sock)
+
 	c, err := net.Dial("unix", sock)
 	require.NoError(t, err)
+
 	client, err := NewClientPipe(c, c)
 	if err != nil {
 		t.Fatalf("unexpected error: %+v", err)
 	}
+
+	<-canReturn
 	pair.svr = server
 	pair.cli = client
 	return pair
