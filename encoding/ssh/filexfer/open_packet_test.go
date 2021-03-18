270384ed193a4a5742786f1726df7efb147a2a19
Cassondra Foesch <puellanivis@gmail.com>
2021-03-18 19:26:55 +0000

    encoding: Opendir

--- encoding/ssh/filexfer/open_packet_test.go
+++ encoding/ssh/filexfer/open_packet_test.go
@@ -68,3 +68,46 @@ func TestOpenPacket(t *testing.T) {
 		t.Errorf("UnmarshalBinary(): Attrs.Permissions was %#x, but expected %#x", p.Attrs.Permissions, perms)
 	}
 }
+
+func TestOpendirPacket(t *testing.T) {
+	const (
+		id   = 42
+		path = "/foo"
+	)
+
+	p := &OpendirPacket{
+		RequestID: id,
+		Path:      path,
+	}
+
+	data, err := p.MarshalBinary()
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	want := []byte{
+		0x00, 0x00, 0x00, 13,
+		11,
+		0x00, 0x00, 0x00, id,
+		0x00, 0x00, 0x00, 4, '/', 'f', 'o', 'o',
+	}
+
+	if !bytes.Equal(data, want) {
+		t.Fatalf("Marshal() = %X, but wanted %X", data, want)
+	}
+
+	*p = OpendirPacket{}
+
+	// UnmarshalBinary assumes the uint32(length) + uint8(type) have already been consumed.
+	if err := p.UnmarshalBinary(data[5:]); err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	if p.RequestID != uint32(id) {
+		t.Errorf("UnmarshalBinary(): RequestID was %d, but expected %d", p.RequestID, id)
+	}
+
+	if p.Path != path {
+		t.Fatalf("UnmarshalBinary(): Path was %q, but expected %q", p.Path, path)
+	}
+}
