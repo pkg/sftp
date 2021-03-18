a62519dc6ddc85ee63f23497f93e080751fcfe8b
Cassondra Foesch <puellanivis@gmail.com>
2021-03-18 16:59:27 +0000

    encoding: import constants

---
+++ encoding/ssh/filexfer/fx.go
@@ -0,0 +1,121 @@
+package filexfer
+
+import (
+	"fmt"
+)
+
+// Status defines the SFTP error codes used in SSH_FXP_STATUS response packets.
+type Status uint8
+
+// Defines the various SSH_FX_* values.
+const (
+	// see draft-ietf-secsh-filexfer-02
+	// https://tools.ietf.org/html/draft-ietf-secsh-filexfer-02#section-7
+	StatusOK = Status(iota)
+	StatusEOF
+	StatusNoSuchFile
+	StatusPermissionDenied
+	StatusFailure
+	StatusBadMessage
+	StatusNoConnection
+	StatusConnectionLost
+	StatusOPUnsupported
+
+	// see draft-ietf-secsh-filexfer-13
+	// https://tools.ietf.org/html/draft-ietf-secsh-filexfer-13#section-9.1
+	// Defined only for interoperability!
+	StatusInvalidHandle
+	StatusNoSuchPath
+	StatusFileAlreadyExists
+	StatusWriteProtect
+	StatusNoMedia
+	StatusNoSpaceOnFilesystem
+	StatusQuotaExceeded
+	StatusUnknownPrincipal
+	StatusLockConflict
+	StatusDirNotEmpty
+	StatusNotADirectory
+	StatusInvalidFilename
+	StatusLinkLoop
+	StatusCannotDelete
+	StatusInvalidParameter
+	StatusFileIsADirectory
+	StatusByteRangeLockConflict
+	StatusByteRangeLockRefused
+	StatusDeletePending
+	StatusFileCorrupt
+	StatusOwnerInvalid
+	StatusGroupInvalid
+	StatusNoMatchingByteRangeLock
+)
+
+func (f Status) String() string {
+	switch f {
+	case StatusOK:
+		return "SSH_FX_OK"
+	case StatusEOF:
+		return "SSH_FX_EOF"
+	case StatusNoSuchFile:
+		return "SSH_FX_NO_SUCH_FILE"
+	case StatusPermissionDenied:
+		return "SSH_FX_PERMISSION_DENIED"
+	case StatusFailure:
+		return "SSH_FX_FAILURE"
+	case StatusBadMessage:
+		return "SSH_FX_BAD_MESSAGE"
+	case StatusNoConnection:
+		return "SSH_FX_NO_CONNECTION"
+	case StatusConnectionLost:
+		return "SSH_FX_CONNECTION_LOST"
+	case StatusOPUnsupported:
+		return "SSH_FX_OP_UNSUPPORTED"
+	case StatusInvalidHandle:
+		return "SSH_FX_INVALID_HANDLE"
+	case StatusNoSuchPath:
+		return "SSH_FX_NO_SUCH_PATH"
+	case StatusFileAlreadyExists:
+		return "SSH_FX_FILE_ALREADY_EXISTS"
+	case StatusWriteProtect:
+		return "SSH_FX_WRITE_PROTECT"
+	case StatusNoMedia:
+		return "SSH_FX_NO_MEDIA"
+	case StatusNoSpaceOnFilesystem:
+		return "SSH_FX_NO_SPACE_ON_FILESYSTEM"
+	case StatusQuotaExceeded:
+		return "SSH_FX_QUOTA_EXCEEDED"
+	case StatusUnknownPrincipal:
+		return "SSH_FX_UNKNOWN_PRINCIPAL"
+	case StatusLockConflict:
+		return "SSH_FX_LOCK_CONFLICT"
+	case StatusDirNotEmpty:
+		return "SSH_FX_DIR_NOT_EMPTY"
+	case StatusNotADirectory:
+		return "SSH_FX_NOT_A_DIRECTORY"
+	case StatusInvalidFilename:
+		return "SSH_FX_INVALID_FILENAME"
+	case StatusLinkLoop:
+		return "SSH_FX_LINK_LOOP"
+	case StatusCannotDelete:
+		return "SSH_FX_CANNOT_DELETE"
+	case StatusInvalidParameter:
+		return "SSH_FX_INVALID_PARAMETER"
+	case StatusFileIsADirectory:
+		return "SSH_FX_FILE_IS_A_DIRECTORY"
+	case StatusByteRangeLockConflict:
+		return "SSH_FX_BYTE_RANGE_LOCK_CONFLICT"
+	case StatusByteRangeLockRefused:
+		return "SSH_FX_BYTE_RANGE_LOCK_REFUSED"
+	case StatusDeletePending:
+		return "SSH_FX_DELETE_PENDING"
+	case StatusFileCorrupt:
+		return "SSH_FX_FILE_CORRUPT"
+	case StatusOwnerInvalid:
+		return "SSH_FX_OWNER_INVALID"
+	case StatusGroupInvalid:
+		return "SSH_FX_GROUP_INVALID"
+	case StatusNoMatchingByteRangeLock:
+		return "SSH_FX_NO_MATCHING_BYTE_RANGE_LOCK"
+	default:
+		return fmt.Sprintf("SSH_FX_UNKNOWN(%d)", f)
+	}
+}
---
+++ encoding/ssh/filexfer/fxp.go
@@ -0,0 +1,117 @@
+package filexfer
+
+import (
+	"fmt"
+)
+
+// PacketType defines the various SFTP packet types.
+type PacketType uint8
+
+// Request packet types.
+const (
+	PacketTypeInit = PacketType(iota + 1)
+	PacketTypeVersion
+	PacketTypeOpen
+	PacketTypeClose
+	PacketTypeRead
+	PacketTypeWrite
+	PacketTypeLstat
+	PacketTypeFstat
+	PacketTypeSetstat
+	PacketTypeFsetstat
+	PacketTypeOpendir
+	PacketTypeReaddir
+	PacketTypeRemove
+	PacketTypeMkdir
+	PacketTypeRmdir
+	PacketTypeRealpath
+	PacketTypeStat
+	PacketTypeRename
+	PacketTypeReadlink
+	PacketTypeSymlink
+	PacketTypeLink
+	PacketTypeBlock
+	PacketTypeUnblock
+)
+
+// Response packet types.
+const (
+	PacketTypeStatus = PacketType(iota + 101)
+	PacketTypeHandle
+	PacketTypeData
+	PacketTypeName
+	PacketTypeAttrs
+)
+
+// Extended packet types.
+const (
+	PacketTypeExtended = PacketType(iota + 200)
+	PacketTypeExtendedReply
+)
+
+func (f PacketType) String() string {
+	switch f {
+	case PacketTypeInit:
+		return "SSH_FXP_INIT"
+	case PacketTypeVersion:
+		return "SSH_FXP_VERSION"
+	case PacketTypeOpen:
+		return "SSH_FXP_OPEN"
+	case PacketTypeClose:
+		return "SSH_FXP_CLOSE"
+	case PacketTypeRead:
+		return "SSH_FXP_READ"
+	case PacketTypeWrite:
+		return "SSH_FXP_WRITE"
+	case PacketTypeLstat:
+		return "SSH_FXP_LSTAT"
+	case PacketTypeFstat:
+		return "SSH_FXP_FSTAT"
+	case PacketTypeSetstat:
+		return "SSH_FXP_SETSTAT"
+	case PacketTypeFsetstat:
+		return "SSH_FXP_FSETSTAT"
+	case PacketTypeOpendir:
+		return "SSH_FXP_OPENDIR"
+	case PacketTypeReaddir:
+		return "SSH_FXP_READDIR"
+	case PacketTypeRemove:
+		return "SSH_FXP_REMOVE"
+	case PacketTypeMkdir:
+		return "SSH_FXP_MKDIR"
+	case PacketTypeRmdir:
+		return "SSH_FXP_RMDIR"
+	case PacketTypeRealpath:
+		return "SSH_FXP_REALPATH"
+	case PacketTypeStat:
+		return "SSH_FXP_STAT"
+	case PacketTypeRename:
+		return "SSH_FXP_RENAME"
+	case PacketTypeReadlink:
+		return "SSH_FXP_READLINK"
+	case PacketTypeSymlink:
+		return "SSH_FXP_SYMLINK"
+	case PacketTypeLink:
+		return "SSH_FXP_LINK"
+	case PacketTypeBlock:
+		return "SSH_FXP_BLOCK"
+	case PacketTypeUnblock:
+		return "SSH_FXP_UNBLOCK"
+	case PacketTypeStatus:
+		return "SSH_FXP_STATUS"
+	case PacketTypeHandle:
+		return "SSH_FXP_HANDLE"
+	case PacketTypeData:
+		return "SSH_FXP_DATA"
+	case PacketTypeName:
+		return "SSH_FXP_NAME"
+	case PacketTypeAttrs:
+		return "SSH_FXP_ATTRS"
+	case PacketTypeExtended:
+		return "SSH_FXP_EXTENDED"
+	case PacketTypeExtendedReply:
+		return "SSH_FXP_EXTENDED_REPLY"
+	default:
+		return fmt.Sprintf("SSH_FXP_UNKNOWN(%d)", f)
+	}
+}
