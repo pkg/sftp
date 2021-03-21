963c7adde77244886dc51c8633bf7595f71a91fd
Cassondra Foesch <puellanivis@gmail.com>
2021-03-21 15:34:35 +0000

    encoding: RequestPacket type to simplify server implementations

--- encoding/ssh/filexfer/packets.go
+++ encoding/ssh/filexfer/packets.go
@@ -1,9 +1,55 @@
 package filexfer
 
 import (
+	"errors"
 	"fmt"
 	"io"
 )
+
+func newPacketFromType(typ PacketType) (Packet, error) {
+	switch typ {
+	case PacketTypeOpen:
+		return new(OpenPacket), nil
+	case PacketTypeClose:
+		return new(ClosePacket), nil
+	case PacketTypeRead:
+		return new(ReadPacket), nil
+	case PacketTypeWrite:
+		return new(WritePacket), nil
+	case PacketTypeLstat:
+		return new(LstatPacket), nil
+	case PacketTypeFstat:
+		return new(FstatPacket), nil
+	case PacketTypeSetstat:
+		return new(SetstatPacket), nil
+	case PacketTypeFsetstat:
+		return new(FsetstatPacket), nil
+	case PacketTypeOpendir:
+		return new(OpendirPacket), nil
+	case PacketTypeReaddir:
+		return new(ReaddirPacket), nil
+	case PacketTypeRemove:
+		return new(RemovePacket), nil
+	case PacketTypeMkdir:
+		return new(MkdirPacket), nil
+	case PacketTypeRmdir:
+		return new(RmdirPacket), nil
+	case PacketTypeRealpath:
+		return new(RealpathPacket), nil
+	case PacketTypeStat:
+		return new(StatPacket), nil
+	case PacketTypeRename:
+		return new(RenamePacket), nil
+	case PacketTypeReadlink:
+		return new(ReadlinkPacket), nil
+	case PacketTypeSymlink:
+		return new(SymlinkPacket), nil
+	case PacketTypeExtended:
+		return new(ExtendedPacket), nil
+	default:
+		return nil, fmt.Errorf("unexpected request packet type: %v", typ)
+	}
+}
 
 // RawPacket implements the general packet format from draft-ietf-secsh-filexfer-02
 //
@@ -60,23 +106,36 @@ func (p *RawPacket) UnmarshalBinary(data []byte) error {
 	return p.UnmarshalFrom(NewBuffer(data))
 }
 
-// ReadFrom reads a full raw packet out of the given reader.
-func (p *RawPacket) ReadFrom(r io.Reader, maxPacketLength uint32) error {
-	lb := make([]byte, 4)
-	if _, err := io.ReadFull(r, lb); err != nil {
-		return err
+// defaultMaxPacketSize is defined in draft-ietf-secsh-filexfer-02 section 3.
+const defaultMaxPacketSize = 34000
+
+// readPacket reads a uint32 length-prefixed binary data packet from r.
+// If the given buffer is less than 4-bytes, it allocates a new buffer of size defaultMaxPacketSize.
+func readPacket(r io.Reader, b []byte) ([]byte, error) {
+	if len(b) < 4 {
+		b = make([]byte, defaultMaxPacketSize)
 	}
 
-	length := unmarshalUint32(lb)
+	if _, err := io.ReadFull(r, b[:4]); err != nil {
+		return nil, err
+	}
+
+	length := unmarshalUint32(b)
 	if length < 1 {
-		return ErrShortPacket
+		return nil, ErrShortPacket
 	}
-	if length > maxPacketLength {
-		return ErrLongPacket
+	if length > uint32(len(b)) {
+		return nil, ErrLongPacket
 	}
 
-	b := make([]byte, length)
-	if _, err := io.ReadFull(r, b); err != nil {
+	n, err := io.ReadFull(r, b[:length])
+	return b[:n], err
+}
+
+// ReadFrom reads a full raw packet out of the given reader.
+func (p *RawPacket) ReadFrom(r io.Reader, b []byte) error {
+	b, err := readPacket(r, b)
+	if err != nil {
 		return err
 	}
 
@@ -84,50 +143,85 @@ func (p *RawPacket) ReadFrom(r io.Reader, maxPacketLength uint32) error {
 }
 
 // RequestPacket decodes a full request packet from the internal Data based on the Type.
-func (p *RawPacket) RequestPacket() (reqid uint32, packet Packet, err error) {
-	switch p.Type {
-	case PacketTypeOpen:
-		packet = new(OpenPacket)
-	case PacketTypeClose:
-		packet = new(ClosePacket)
-	case PacketTypeRead:
-		packet = new(ReadPacket)
-	case PacketTypeWrite:
-		packet = new(WritePacket)
-	case PacketTypeLstat:
-		packet = new(LstatPacket)
-	case PacketTypeFstat:
-		packet = new(FstatPacket)
-	case PacketTypeSetstat:
-		packet = new(SetstatPacket)
-	case PacketTypeFsetstat:
-		packet = new(FsetstatPacket)
-	case PacketTypeOpendir:
-		packet = new(OpendirPacket)
-	case PacketTypeReaddir:
-		packet = new(ReaddirPacket)
-	case PacketTypeRemove:
-		packet = new(RemovePacket)
-	case PacketTypeMkdir:
-		packet = new(MkdirPacket)
-	case PacketTypeRmdir:
-		packet = new(RmdirPacket)
-	case PacketTypeRealpath:
-		packet = new(RealpathPacket)
-	case PacketTypeStat:
-		packet = new(StatPacket)
-	case PacketTypeRename:
-		packet = new(RenamePacket)
-	case PacketTypeReadlink:
-		packet = new(ReadlinkPacket)
-	case PacketTypeSymlink:
-		packet = new(SymlinkPacket)
-	case PacketTypeExtended:
-		packet = new(ExtendedPacket)
-	default:
-		return p.RequestID, nil, fmt.Errorf("unexpected packet request type: %v", p.Type)
+func (p *RawPacket) RequestPacket() (*RequestPacket, error) {
+	packet, err := newPacketFromType(p.Type)
+	if err != nil {
+		return nil, err
 	}
 
 	packet.UnmarshalPacketBody(&p.Data)
-	return p.RequestID, packet, err
+
+	return &RequestPacket{
+		RequestID: p.RequestID,
+
+		Request: packet,
+	}, nil
+}
+
+// RequestPacket implements the general packet format from draft-ietf-secsh-filexfer-02
+//
+// Defined in https://tools.ietf.org/html/draft-ietf-secsh-filexfer-02#section-3
+type RequestPacket struct {
+	RequestID uint32
+
+	Request Packet
+}
+
+// Reset clears the pointers and reference-semantic variables of RequestPacket,
+// making it suitable to be put into a sync.Pool.
+func (p *RequestPacket) Reset() {
+	p.Request = nil
+}
+
+// MarshalPacket returns p as a two-part binary encoding of p.
+//
+// The internal p.RequestID is overridden by the reqid argument.
+func (p *RequestPacket) MarshalPacket(reqid uint32) (header, payload []byte, err error) {
+	if p.Request == nil {
+		return nil, nil, errors.New("empty request packet")
+	}
+
+	return p.Request.MarshalPacket(reqid)
+}
+
+// MarshalBinary returns p as the binary encoding of p.
+func (p *RequestPacket) MarshalBinary() ([]byte, error) {
+	return ComposePacket(p.MarshalPacket(p.RequestID))
+}
+
+// UnmarshalFrom decodes a RequestPacket from the given Buffer into p.
+func (p *RequestPacket) UnmarshalFrom(buf *Buffer) error {
+	typ, err := buf.ConsumeUint8()
+	if err != nil {
+		return err
+	}
+
+	p.Request, err = newPacketFromType(PacketType(typ))
+	if err != nil {
+		return err
+	}
+
+	if p.RequestID, err = buf.ConsumeUint32(); err != nil {
+		return err
+	}
+
+	return p.Request.UnmarshalPacketBody(buf)
+}
+
+// UnmarshalBinary decodes a full raw packet out of the given data.
+// It is assumed that the uint32(length) has already been consumed to receive the data.
+//
+// NOTE: To avoid extra allocations, UnmarshalBinary aliases the given byte slice.
+func (p *RequestPacket) UnmarshalBinary(data []byte) error {
+	return p.UnmarshalFrom(NewBuffer(data))
+}
+
+// ReadFrom reads a full raw packet out of the given reader.
+func (p *RequestPacket) ReadFrom(r io.Reader, b []byte) error {
+	b, err := readPacket(r, b)
+	if err != nil {
+		return err
+	}
+
+	return p.UnmarshalBinary(b)
 }
--- encoding/ssh/filexfer/packets_test.go
+++ encoding/ssh/filexfer/packets_test.go
@@ -6,11 +6,14 @@ import (
 )
 
 func TestRawPacket(t *testing.T) {
-	var id byte = 42
+	const (
+		id   = 42
+		path = "foo"
+	)
 
 	p := &RawPacket{
 		Type:      PacketTypeStat,
-		RequestID: uint32(id),
+		RequestID: id,
 		Data: Buffer{
 			b: []byte{0x00, 0x00, 0x00, 0x03, 'f', 'o', 'o'},
 		},
@@ -24,7 +27,7 @@ func TestRawPacket(t *testing.T) {
 	want := []byte{
 		0x00, 0x00, 0x00, 12,
 		17,
-		0x00, 0x00, 0x00, id,
+		0x00, 0x00, 0x00, 42,
 		0x00, 0x00, 0x00, 3, 'f', 'o', 'o',
 	}
 
@@ -34,8 +37,7 @@ func TestRawPacket(t *testing.T) {
 
 	*p = RawPacket{}
 
-	// UnmarshalBinary assumes the length has already been consumed.
-	if err := p.UnmarshalBinary(data[4:]); err != nil {
+	if err := p.ReadFrom(bytes.NewReader(data), nil); err != nil {
 		t.Fatal("unexpected error:", err)
 	}
 
@@ -53,5 +55,72 @@ func TestRawPacket(t *testing.T) {
 
 	if !bytes.Equal(p.Data.Bytes(), want) {
 		t.Errorf("RawPacket.UnmarshalBinary(): Data was %X, but expected %X", p.Data, want)
+	}
+
+	rp, err := p.RequestPacket()
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	if rp.RequestID != uint32(id) {
+		t.Errorf("RawPacket.RequestPacket(): RequestID was %d, but expected %d", rp.RequestID, id)
+	}
+
+	req, ok := rp.Request.(*StatPacket)
+	if !ok {
+		t.Fatalf("unexpected Request type was %T, but expected %T", rp.Request, req)
+	}
+
+	if req.Path != path {
+		t.Errorf("RawPacket.RequestPacket(): Request.Path was %q, but expected %q", req.Path, path)
+	}
+}
+
+func TestRequestPacket(t *testing.T) {
+	const (
+		id   = 42
+		path = "foo"
+	)
+
+	p := &RequestPacket{
+		RequestID: id,
+		Request: &StatPacket{
+			Path: path,
+		},
+	}
+
+	data, err := p.MarshalBinary()
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	want := []byte{
+		0x00, 0x00, 0x00, 12,
+		17,
+		0x00, 0x00, 0x00, 42,
+		0x00, 0x00, 0x00, 3, 'f', 'o', 'o',
+	}
+
+	if !bytes.Equal(data, want) {
+		t.Errorf("RequestPacket.Marshal() = %X, but wanted %X", data, want)
+	}
+
+	*p = RequestPacket{}
+
+	if err := p.ReadFrom(bytes.NewReader(data), nil); err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+
+	if p.RequestID != uint32(id) {
+		t.Errorf("RequestPacket.UnmarshalBinary(): RequestID was %d, but expected %d", p.RequestID, id)
+	}
+
+	req, ok := p.Request.(*StatPacket)
+	if !ok {
+		t.Fatalf("unexpected Request type was %T, but expected %T", p.Request, req)
+	}
+
+	if req.Path != path {
+		t.Errorf("RequestPacket.UnmarshalBinary(): Request.Path was %q, but expected %q", req.Path, path)
 	}
 }
