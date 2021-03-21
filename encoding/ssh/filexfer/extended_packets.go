229257daaf43d0e6d928f85cdbb5ba91ccdf622a
Cassondra Foesch <puellanivis@gmail.com>
2021-03-21 18:02:15 +0000

    enocoding: refactor extended packets

--- encoding/ssh/filexfer/extended_packets.go
+++ encoding/ssh/filexfer/extended_packets.go
@@ -2,12 +2,40 @@ package filexfer
 
 import (
 	"encoding"
+	"sync"
 )
 
 // ExtendedData aliases the untyped interface composition of encoding.BinaryMarshaler and encoding.BinaryUnmarshaler.
 type ExtendedData = interface {
 	encoding.BinaryMarshaler
 	encoding.BinaryUnmarshaler
+}
+
+type ExtendedDataConstructor func() ExtendedData
+
+var extendedPacketTypes = struct {
+	mu           sync.RWMutex
+	constructors map[string]ExtendedDataConstructor
+}{
+	constructors: make(map[string]ExtendedDataConstructor),
+}
+
+func RegisterExtendedPacketType(extension string, constructor ExtendedDataConstructor) {
+	extendedPacketTypes.mu.Lock()
+	defer extendedPacketTypes.mu.Unlock()
+
+	extendedPacketTypes.constructors[extension] = constructor
+}
+
+func newExtendedPacket(extension string) ExtendedData {
+	extendedPacketTypes.mu.RLock()
+	defer extendedPacketTypes.mu.RUnlock()
+
+	if f := extendedPacketTypes.constructors[extension]; f != nil {
+		return f()
+	}
+
+	return new(Buffer)
 }
 
 // ExtendedPacket defines the SSH_FXP_CLOSE packet.
@@ -40,24 +68,19 @@ func (p *ExtendedPacket) MarshalPacket(reqid uint32) (header, payload []byte, er
 // UnmarshalPacketBody unmarshals the packet body from the given Buffer.
 // It is assumed that the uint32(request-id) has already been consumed.
 //
-// If p.Data is nil, and there is request-specific-data,
-// then the request-specific-data will be wrapped in a Buffer and assigned to p.Data.
+// If p.Data is nil, and the extension has been registered, a new type will be made from the registration.
+// If the extension has not been registered, then a new Buffer will be allocated.
+// Then the request-specific-data will be unmarshaled from the rest of the buffer.
 func (p *ExtendedPacket) UnmarshalPacketBody(buf *Buffer) (err error) {
 	if p.ExtendedRequest, err = buf.ConsumeString(); err != nil {
 		return err
 	}
 
-	if buf.Len() > 0 {
-		if p.Data == nil {
-			p.Data = new(Buffer)
-		}
-
-		if err := p.Data.UnmarshalBinary(buf.Bytes()); err != nil {
-			return err
-		}
+	if p.Data == nil {
+		p.Data = newExtendedPacket(p.ExtendedRequest)
 	}
 
-	return nil
+	return p.Data.UnmarshalBinary(buf.Bytes())
 }
 
 // ExtendedReplyPacket defines the SSH_FXP_CLOSE packet.
@@ -87,15 +110,9 @@ func (p *ExtendedReplyPacket) MarshalPacket(reqid uint32) (header, payload []byt
 // If p.Data is nil, and there is request-specific-data,
 // then the request-specific-data will be wrapped in a Buffer and assigned to p.Data.
 func (p *ExtendedReplyPacket) UnmarshalPacketBody(buf *Buffer) (err error) {
-	if buf.Len() > 0 {
-		if p.Data == nil {
-			p.Data = new(Buffer)
-		}
-
-		if err := p.Data.UnmarshalBinary(buf.Bytes()); err != nil {
-			return err
-		}
+	if p.Data == nil {
+		p.Data = new(Buffer)
 	}
 
-	return nil
+	return p.Data.UnmarshalBinary(buf.Bytes())
 }
