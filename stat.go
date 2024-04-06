3588ee80db20622709895556868ed7c0d2613abc
dustin-ward <wardddustin@gmail.com>
2024-04-05 22:11:36 -0400

    fix godoc comment

--- stat.go
+++ stat.go
@@ -85,9 +85,5 @@ const (
 	s_ISVTX = uint32(sshfx.ModeSticky)
 )
 
-// Legacy export:
-//
-// Go defines S_IFMT on windows, plan9 and js/wasm as 0x1f000 instead of
-// 0xf000. None of the the other S_IFxyz values include the "1" (in 0x1f000)
-// which prevents them from matching the bitmask.
+// S_IFMT is a legacy export. `sshfx.ModeType` should be used instead
 const S_IFMT = uint32(sshfx.ModeType)
